f5a1b5f4c9f40ea1d36b032539d5ca7f6d5593b0
HeberthRodriguez <heberthrodriguez10@gmail.com>
2025-04-30 19:35:10 -0600

    Update hilos.java

--- hilos.java
+++ hilos.java
@@ -3,4 +3,5 @@ Laboratorio de  gitHubgit commit -m "first commit"
 public static void main (String[] args){
         System.out.println("hola mundo");
         System.out.println("holaA");
+        System.out.println("hola sapo");
 }
